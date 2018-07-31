09d1d2968ac22a0480ff031eb643a4a4bd86c9d7
Mike <m.g.johnson0626@gmail.com>
2018-07-31 13:26:36 -0500

    final

--- EX15.js
+++ EX15.js
@@ -1,2 +1,17 @@
-var num = prompt("Enter your fav number");
-console.log(num);
+function go(){
+    var num = prompt("Enter your fav number");
+    alert(num);
+    aboveOrBelow100();
+}
+
+function aboveOrBelow100(){
+    if (num <100);
+        alert("Less than a Hunnid")
+    if (num> 100);
+        alert("More than a Hunnid")
+    if (num == 100);
+        alert("Is a Hunnid");
+
+
+}
+
